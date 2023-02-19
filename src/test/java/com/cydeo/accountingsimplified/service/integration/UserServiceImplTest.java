d0e3f29797cf5fbbede2d5c6c5545041e3e4ec79
owermaiden <ower.maiden@cybertekschool.com>
2023-02-20 00:09:52 +0100

    docker compose

--- src/test/java/com/cydeo/accountingsimplified/service/integration/UserServiceImplTest.java
+++ src/test/java/com/cydeo/accountingsimplified/service/integration/UserServiceImplTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
