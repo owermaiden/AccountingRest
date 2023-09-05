5b6aded24676e6023ba45e24cbc50cb0c8d9a40a
owermaiden <ower.maiden@cybertekschool.com>
2023-09-04 23:17:09 -0400

    swagger added

--- src/main/java/com/cydeo/accountingsimplified/config/SwaggerConfig.java
+++ src/main/java/com/cydeo/accountingsimplified/config/SwaggerConfig.java
@@ -5,8 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig  {
@@ -17,7 +21,18 @@ public class SwaggerConfig  {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.cydeo.accountingsimplified.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Accounting REST API",
+                "Cydeo",
+                "API V1",
+                "Terms of service",
+                new Contact("John Doe", "www.example.com", "myeaddress@company.com"),
+                "License of API", "API license URL", Collections.emptyList());
     }
 
 }
